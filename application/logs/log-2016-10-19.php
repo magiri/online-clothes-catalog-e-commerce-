167cce5cbdea5902af06315cf5ebaebf7388f618
smartin smart <smartin smart>
2016-10-19 09:33:19 +0300

    First Commit

---
+++ application/logs/log-2016-10-19.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-10-19 06:29:59 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead A:\wamp\www\faithknits\system\database\drivers\mysql\mysql_driver.php 91
+ERROR - 2016-10-19 06:30:07 --> 404 Page Not Found --> custompage
+ERROR - 2016-10-19 06:30:07 --> 404 Page Not Found --> custompage
+ERROR - 2016-10-19 06:30:08 --> 404 Page Not Found --> custompage
+ERROR - 2016-10-19 06:30:10 --> 404 Page Not Found --> custompage
+ERROR - 2016-10-19 06:30:10 --> 404 Page Not Found --> custompage
+ERROR - 2016-10-19 06:30:12 --> 404 Page Not Found --> custompage
